0bd472c619a1fe1ca63cc985030ecf419d7348a1
Firdaous <srififirdaous@gmail.com>
2024-12-25 13:43:19 +0100

    added

---
+++ modules/restaurants.js
@@ -0,0 +1,5 @@
+const restaurants = require('data/data.json').restaurants;
+
+module.exports = {
+    restaurants
+};
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const restaurants = require();
+const restaurants = require('modules/restaurants.js');
+
+app.listen(parent, () => {
+    console.log(`Server running on port ${port}`);
+
+})
